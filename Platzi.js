3011d9fa44a92b6107f344bad1f0c8979aa0d619
Nicolas <nicolasalbornoz32@gmail.com>
2018-10-05 15:39:50 -0300

    Subo la estructura del proyecto al repositorio de GitHub

---
+++ Platzi.js
@@ -0,0 +1,84 @@
+var vp = document.getElementById("villaplatzi");
+var papel = vp.getContext("2d");
+
+var fondo = {
+    url: "tile.png",
+    cargaOK: false
+};
+var vaca = {
+    url: "vaca.png",
+    cargaOK: false
+};
+var cerdo = {
+    url: "cerdo.png",
+    cargaOK: false
+};
+var pollo = {
+    url: "pollo.png",
+    cargaOK: false
+};
+
+fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVacas);
+
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
+
+function cargarFondo()
+{
+    fondo.cargaOK = true;
+    dibujar();
+}
+function cargarVacas()
+{
+    vaca.cargaOK = true;
+    dibujar();
+}
+function cargarCerdos()
+{
+    cerdo.cargaOK = true;
+    dibujar();
+}
+function cargarPollos()
+{
+    pollo.cargaOK = true;
+    dibujar();
+}
+
+
+function dibujar()
+{
+    if(fondo.cargaOK == true)
+    {
+        papel.drawImage(fondo.imagen, 0, 0);
+    }
+    if(vaca.cargaOK == true)
+    {
+        papel.drawImage(vaca.imagen, 90, 0);
+    }
+    if(cerdo.cargaOK == true)
+    {
+        papel.drawImage(cerdo.imagen, 30, 20);
+    }
+    if(pollo.cargaOK == true)
+    {
+        papel.drawImage(pollo.imagen, 80, 80);
+    }
+}
+
+function aleatorio(min, maxi)
+{
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+    return resultado;
+}
